89efd02aafa6d2352772fe127a419044d410b9c0
Vishal Fenn <vishal.fenn.18@cnu.edu>
2020-11-15 18:43:56 -0500

    Completed project 4

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -12,99 +12,93 @@
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
-using namespace KP_StringParserClass;
-StringParserClass::StringParserClass(void) {
-	pEndTag = NULL;
-	pStartTag = NULL;
-	areTagsSet = false;
-}
+namespace KP_StringParserClass {
+	StringParserClass::StringParserClass() {
+		pEndTag = NULL;
+		pStartTag = NULL;
+		areTagsSet = false;
+	}
 
 //call cleanup to release any allocated memory
-StringParserClass::~StringParserClass(void) {
-	cleanup();
-
-}
-
-int StringParserClass::setTags(const char *pStart, const char *pEnd) {
-	if (pStart == NULL || pEnd == NULL) {
-		return (ERROR_TAGS_NULL);
+	StringParserClass::~StringParserClass(void) {
+		cleanup();
 	}
 
-	pStartTag = new char[strlen(pStart) + 1];
-	pEndTag = new char[strlen(pEnd) + 1];
-	strncpy(pStartTag, pStart, strlen(pStart));
-	strncpy(pEndTag, pEnd, strlen(pEnd));
+	int StringParserClass::setTags(const char *pStart, const char *pEnd) {
 
-	areTagsSet = true;
-	return (SUCCESS);
-
-}
-
-int StringParserClass::getDataBetweenTags(char *pDataToSearchThru,
-        vector<string> &myVector) {
-
-	myVector.clear();
-
-	if (pStartTag == NULL || pEndTag == NULL) {
-		return (ERROR_TAGS_NULL);
-	}
-	if (pDataToSearchThru == NULL) {
-		return (ERROR_DATA_NULL);
-	}
-	char *start1Ptr = pDataToSearchThru;
-	char *end1Ptr;
-	char *start2Ptr = pDataToSearchThru;
-	char *end2Ptr;
-
-	while (findTag(pStartTag, start1Ptr, end1Ptr) == SUCCESS
-	        && findTag(pEndTag, start2Ptr, end2Ptr) == SUCCESS) {
-		string data = "";
-		++end1Ptr;
-		while (end1Ptr != start2Ptr) {
-			data += *end1Ptr;
-			++end1Ptr;
+		if (pStart == NULL || pEnd == NULL) {
+			return (ERROR_TAGS_NULL);
 		}
-		myVector.push_back(data);
-		start1Ptr = end2Ptr;
-		end1Ptr = start1Ptr;
+
+		pEndTag = (char*) malloc(strlen(pStart) * sizeof(char));
+		pStartTag = (char*) malloc(strlen(pEnd) * sizeof(char));
+
+		strcpy(pStartTag, pStart);
+		strcpy(pEndTag, pEnd);
+
+		return (SUCCESS);
 	}
 
-	return (SUCCESS);
-}
+	int KP_StringParserClass::StringParserClass::getDataBetweenTags(
+			char *pDataToSearchThru, vector<string> &myVector) {
 
-void StringParserClass::cleanup() {
-	if (pStartTag) {
-		delete[] pStartTag;
-		pStartTag = 0;
-	}
-	if (pEndTag) {
-		delete[] pEndTag;
-		pEndTag = 0;
-	}
+		if (pStartTag == NULL || pEndTag == NULL) {
+			return (ERROR_TAGS_NULL);
+		}
 
-}
-int StringParserClass::findTag(char *pTagToLookFor, char *&pStart,
-        char *&pEnd) {
+		if (pDataToSearchThru == NULL) {
+			return (ERROR_DATA_NULL);
+		}
 
-	if (pStart == NULL || pEnd == NULL) {
-		return (ERROR_TAGS_NULL);
-	}
-	pEnd = pStart;
+		myVector.clear();
+		char *startTag1 = pDataToSearchThru;
+		char *startTag2;
 
-	while (*pEnd != '\0') {
-		if (*pStart == *pTagToLookFor) {
-			char *tagTemp = pTagToLookFor;
-			while (*pEnd == *tagTemp) {
-				++tagTemp;
-				if (*tagTemp == '\0') {
-					return (SUCCESS);
-				}
-				++pEnd;
+		char *endTag1 = pDataToSearchThru;
+		char *endTag2;
+
+		while (findTag(pStartTag, startTag1, startTag2) == SUCCESS
+				&& findTag(pEndTag, endTag1, endTag2) == SUCCESS) {
+			string data = "";
+			++startTag2;
+			while (startTag2 != endTag1) {
+				data += *startTag2;
+				++startTag2;
 			}
+			myVector.push_back(data);
+			startTag1 = endTag2++;
+			endTag1 = startTag1;
 		}
-		pStart++;
-		pEnd = pStart;
-	}
-	return (FAIL);
-}
 
+		return (SUCCESS);
+	}
+
+	void KP_StringParserClass::StringParserClass::cleanup() {
+
+	}
+
+	int KP_StringParserClass::StringParserClass::findTag(char *pTagToLookFor,
+			char *&pStart, char *&pEnd) {
+
+		if (pStart == NULL || pEnd == NULL) {
+			return (ERROR_TAGS_NULL);
+		}
+		pEnd = pStart;
+		while (*pEnd != '\0') {
+			if (*pStart == *pTagToLookFor) {
+				char *temp = pTagToLookFor;
+				while (*pEnd == *temp) {
+					++temp;
+					if (*temp == '\0') {
+						return (SUCCESS);
+					}
+					++pEnd;
+				}
+			}
+			++pStart;
+			pEnd = pStart;
+		}
+
+		return (FAIL);
+	}
+}
